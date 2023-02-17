00e430b088b843314f5eb05cfdc2a1c570cce66a
Tatiana_Solorzano <tatianasolorzanoa@gmail.com>
2023-02-17 12:35:06 -0500

    deploy

--- django_portfolio/urls.py
+++ django_portfolio/urls.py
@@ -25,7 +25,7 @@ from portfolio import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
 ]
 
 
